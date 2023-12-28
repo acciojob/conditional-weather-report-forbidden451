d29b2e45581e457d1b10b06a6896adaed676ccf9
Deepak <kanshettid@gmail.com>
2023-12-28 22:38:09 +0530

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,27 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const weatherInput = {temperature:25,conditions:"Sunny"};
+  const thresholdTemperature = 20;
+  
+  function getColourOnTemperature(temperature){
+    return temperature>thresholdTemperature?"red":"blue";
+  }
+
+  let color="";
+  function displayWeatherReport(weatherInput){
+    color = getColourOnTemperature(weatherInput.temperature);
+  }
+
+
+  displayWeatherReport(weatherInput);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p style={{color:color}}>Temperature:{weatherInput.temperature}</p>
+      <p style={{color:color}}>Conditions:{weatherInput.conditions}</p>
     </div>
   )
 }
 
-export default App
+export default App;
